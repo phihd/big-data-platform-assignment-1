286504f958f830a0358c39c924a14f2eeed087e9
phihd <phictin@gmail.com>
2022-02-18 12:22:40 +0200

    fix daas

--- code/mysimbdp-daas.py
+++ code/mysimbdp-daas.py
@@ -6,6 +6,7 @@ from flask_restx import Resource, Api
 client = pymongo.MongoClient('mongodb+srv://phihd:iXkIXCNJhQNQriwF@cluster0.vwz1m.mongodb.net/bdp-a1?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE')
 database = client['bdp-a1']
 covid_table = database['covid19']
+covid_tables = [database['covid19'], database['covid19_1'], database['covid19_2']]
 
 app = Flask(__name__)
 api = Api(app)
@@ -18,13 +19,14 @@ def post():
 
 @app.route('/clear', methods=['POST'])
 def clear():
-	covid_table.delete_many(request.json)
+	for _covid_table in covid_tables:
+		_covid_table.delete_many(request.json)
 	return ''
 
 @covid_api.route('/find/date=<date>&country=<country>', methods=['GET'])
 class find(Resource):
 	def get(self, date, country):
-		covid_table.create_index('dateRep')
+		covid_table.create_index(['dateRep','countriesAndTerritories'])
 		formatted_date = date.replace("-", "/")
 		result = list(covid_table.find({'dateRep':formatted_date, 'countriesAndTerritories':country}))
 		return dumps(result)
